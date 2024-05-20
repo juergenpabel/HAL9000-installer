22348d41b968e7df1a96f9d24e9bdea386a4b5af
Juergen Pabel <juergen@pabel.net>
2024-05-20 05:25:49 +0200

    added containers-storage to install_packages.sh because of performance benefits from overlayfs

--- resources/scripts/linux/software/install_packages.sh
+++ resources/scripts/linux/software/install_packages.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y python3 python3-venv python3-pip-whl podman
+sudo apt install -y python3 python3-venv python3-pip-whl podman containers-storage
 
