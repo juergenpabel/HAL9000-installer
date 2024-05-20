9d76d19a0a55dc12f556a2dbbc946285cead983b
Juergen Pabel <juergen@pabel.net>
2024-05-20 06:25:53 +0200

    adapted udev triggers

--- resources/scripts/linux/configure/create_udev_alsa.sh
+++ resources/scripts/linux/configure/create_udev_alsa.sh
@@ -3,7 +3,7 @@
 echo "HAL9000: Configuring udev for ALSA:HAL9000..."
 if [ ! -f /etc/udev/rules.d/99-hal9000-alsa.rules ]; then
         sudo sh -c 'echo "SUBSYSTEM!=\"sound\", GOTO=\"hal9000_alsa_end\""                                              >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-        sudo sh -c 'echo "ACTION!=\"add\", GOTO=\"hal9000_alsa_end\""                                                   >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+        sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_alsa_end\""                                            >> /etc/udev/rules.d/99-hal9000-alsa.rules'
         sudo sh -c 'echo "DEVPATH==\"/devices/platform/soc/soc:sound/sound/card?\", ATTR{id}=\"HAL9000\""               >> /etc/udev/rules.d/99-hal9000-alsa.rules'
         sudo sh -c 'echo "DEVPATH==\"/devices/pci0000:00/0000:00:08.1/0000:c1:00.6/sound/card?\", ATTR{id}=\"HAL9000\"" >> /etc/udev/rules.d/99-hal9000-alsa.rules'
         sudo sh -c 'echo "LABEL=\"hal9000_alsa_end\""                                                                   >> /etc/udev/rules.d/99-hal9000-alsa.rules'
--- resources/scripts/linux/configure/create_udev_tty.sh
+++ resources/scripts/linux/configure/create_udev_tty.sh
@@ -3,7 +3,7 @@
 echo "HAL9000: Configuring udev for /dev/ttyHAL9000..."
 if [ ! -f /etc/udev/rules.d/99-hal9000-tty.rules ]; then
         sudo sh -c 'echo "SUBSYSTEM!=\"tty\", GOTO=\"hal9000_tty_end\""                                   >>  /etc/udev/rules.d/99-hal9000-tty.rules'
-        sudo sh -c 'echo "ACTION!=\"add\", GOTO=\"hal9000_tty_end\""                                      >>  /etc/udev/rules.d/99-hal9000-tty.rules'
+        sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_tty_end\""                               >>  /etc/udev/rules.d/99-hal9000-tty.rules'
         sudo sh -c 'echo "ATTRS{idVendor}==\"2e8a\", ATTRS{idProduct}==\"000a\", SYMLINK+=\"ttyHAL9000\"" >>  /etc/udev/rules.d/99-hal9000-tty.rules'
         sudo sh -c 'echo "ATTRS{idVendor}==\"1a86\", ATTRS{idProduct}==\"55d4\", SYMLINK+=\"ttyHAL9000\"" >>  /etc/udev/rules.d/99-hal9000-tty.rules'
         sudo sh -c 'echo "LABEL=\"hal9000_tty_end\""                                                      >>  /etc/udev/rules.d/99-hal9000-tty.rules'
