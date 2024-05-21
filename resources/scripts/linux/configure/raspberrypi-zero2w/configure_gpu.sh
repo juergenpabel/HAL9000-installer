c33d3366d1bc9bbd7d29957fdcb87b3283cbdfb4
Juergen Pabel <juergen@pabel.net>
2024-05-21 17:22:32 +0200

    scripts: fixed sed invocations

--- resources/scripts/linux/configure/raspberrypi-zero2w/configure_gpu.sh
+++ resources/scripts/linux/configure/raspberrypi-zero2w/configure_gpu.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-grep -q '^gpu_mem=' /boot/firmware/config.txt >/dev/null
-if [ $? -eq 0 ]; then
-	sudo sed -i 's/^gpu_mem=\d+$/gpu_mem=16/' /boot/firmware/config.txt
-else
-	sudo sh -c 'echo \"gpu_mem=16\" >> /boot/firmware/config.txt'
+RPI_GPU_MB=${1:-16}
+
+if [ -f /boot/firmware/config.txt ]; then
+	grep -q '^gpu_mem=' /boot/firmware/config.txt >/dev/null
+	if [ $? -eq 0 ]; then
+		sudo sed -i "s/^gpu_mem=[[:digit:]]\\+$/gpu_mem=${RPI_GPU_MB}/" /boot/firmware/config.txt
+	else
+		sudo sh -c "echo \"gpu_mem=${RPI_GPU_MB}\" >> /boot/firmware/config.txt"
+	fi
 fi
 
--- resources/scripts/linux/configure/raspberrypi-zero2w/configure_maxcpus.sh
+++ resources/scripts/linux/configure/raspberrypi-zero2w/configure_maxcpus.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-grep -q 'maxcpus=' /boot/firmware/cmdline.txt >/dev/null
-if [ $? -ne 0 ]; then
-	sudo sed -i 's/$/ maxcpus=2/' /boot/firmware/cmdline.txt
+RPI_MAX_CPUS=${1:-2}
+
+if [ -f /boot/firmware/cmdline.txt ]; then
+	grep -q 'maxcpus=' /boot/firmware/cmdline.txt >/dev/null
+	if [ $? -ne 0 ]; then
+		sudo sed -i "s/\$/ maxcpus=${RPI_MAX_CPUS}/" /boot/firmware/cmdline.txt
+	else
+		sudo sed -i "s/ maxcpus=[[:digit:]]\\+/ maxcpus=${RPI_MAX_CPUS}/" /boot/firmware/cmdline.txt
+	fi
 fi
 
