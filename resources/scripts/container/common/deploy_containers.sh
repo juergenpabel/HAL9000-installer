9e2d50e132b698e27bce8a7a9759595f6bd9d4f3
Juergen Pabel <juergen@pabel.net>
2024-05-21 16:52:55 +0200

    scripts: fixed sudo invocation in deploy_containers.sh

--- resources/scripts/container/common/deploy_containers.sh
+++ resources/scripts/container/common/deploy_containers.sh
@@ -7,10 +7,12 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Generating systemd files (in ~hal9000/.config/systemd/user)..."
-sudo -i -u hal9000 sh -c 'mkdir -p ~hal9000/.config/systemd/user'
-cd ~/.config/systemd/user
-sudo -i -u hal9000 podman generate systemd -n -f --start-timeout 5 --stop-timeout 5 hal9000
-cd - >/dev/null
+sudo -i -u hal9000 test -d ~hal9000/.config/systemd/user
+if [ $? -ne 0 ]; then
+	sudo -i -u hal9000 mkdir -p ~hal9000/.config/systemd/user
+fi
+sudo -i -u hal9000 sh -c "cd ~hal9000/.config/systemd/user ; \
+                          podman generate systemd -n -f --start-timeout 5 --stop-timeout 5 hal9000"
 
 echo "Reloading systemd (user instance)..."
 sudo -i -u hal9000 systemctl --user daemon-reload
