7cdea7ea70c848c9bbba5ac6f809358912f2b245
Juergen Pabel <juergen@pabel.net>
2024-05-22 12:31:09 +0200

    updated submodule resources/repositories/HAL9000

--- resources/scripts/container/ghcr.io/download_images.sh
+++ resources/scripts/container/ghcr.io/download_images.sh
@@ -5,6 +5,6 @@ TAG=${1:-latest}
 echo "Downloading container images..."
 for NAME in mosquitto kalliope frontend console brain ; do
         echo "- ghcr.io/juergenpabel/hal9000-$NAME:$TAG ..."
-        sudo -u hal9000 -i podman pull ghcr.io/juergenpabel/hal9000-$NAME:$TAG
+        sudo -i -u hal9000 podman pull ghcr.io/juergenpabel/hal9000-$NAME:$TAG
 done
 
