98242891216a626037c697f62d0797a0cc5a8323
Juergen Pabel <juergen@pabel.net>
2024-05-28 15:44:35 +0200

    install_fake_nproc.sh: fixed typo

--- resources/scripts/linux/software/common/install_fake_nproc.sh
+++ resources/scripts/linux/software/common/install_fake_nproc.sh
@@ -4,8 +4,8 @@ NUM_CPU=${1:-1}
 
 echo "HAL9000: Installing fake 'nproc' in /usr/local/bin to avoid system hangs..."
 if [ ! -f /usr/local/bin/nproc ]; then
-	sudo -c 'echo "#!/bin/sh"            >  /usr/local/bin/nproc'
-	sudo -c 'echo "echo '${NUM_CPU}'"    >> /usr/local/bin/nproc'
+	sudo sh -c 'echo "#!/bin/sh"         >  /usr/local/bin/nproc'
+	sudo sh -c 'echo "echo '${NUM_CPU}'" >> /usr/local/bin/nproc'
 	chmod 755 /usr/local/bin/nproc
 fi
 
