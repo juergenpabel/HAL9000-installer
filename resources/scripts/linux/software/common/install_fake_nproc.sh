77469e15897f902db385ffa1f421c11a56caa68d
Juergen Pabel <juergen@pabel.net>
2024-05-28 15:47:22 +0200

    install_fake_nproc.sh: added missing sudo

--- resources/scripts/linux/software/common/install_fake_nproc.sh
+++ resources/scripts/linux/software/common/install_fake_nproc.sh
@@ -6,6 +6,6 @@ echo "HAL9000: Installing fake 'nproc' in /usr/local/bin to avoid system hangs..
 if [ ! -f /usr/local/bin/nproc ]; then
 	sudo sh -c 'echo "#!/bin/sh"         >  /usr/local/bin/nproc'
 	sudo sh -c 'echo "echo '${NUM_CPU}'" >> /usr/local/bin/nproc'
-	chmod 755 /usr/local/bin/nproc
+	sudo chmod 755 /usr/local/bin/nproc
 fi
 
