14f1306084a00fef0590f7e9db65747acb2da6a8
Juergen Pabel <juergen@pabel.net>
2024-05-29 18:21:36 +0200

    scripts: container scripts don't need a prepare-script, removed

--- resources/scripts/container/ghcr.io/run.sh
+++ resources/scripts/container/ghcr.io/run.sh
@@ -7,7 +7,6 @@ echo "HAL9000: Downloading images and deploying containers..."
 SCRIPT_SRC=`realpath -s $0`
 SCRIPT_DIR=`dirname "$SCRIPT_SRC"`
 
-$SCRIPT_DIR/prepare_env.sh
 $SCRIPT_DIR/download_images.sh $IMAGE_TAG
 $SCRIPT_DIR/create_containers.sh ghcr.io/juergenpabel $IMAGE_TAG
 $SCRIPT_DIR/deploy_containers.sh ghcr.io/juergenpabel $IMAGE_TAG
