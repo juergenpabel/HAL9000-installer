3d273abf0dc9972abdc86fd36acc43c4d477ee5c
Juergen Pabel <juergen@pabel.net>
2024-05-30 13:56:42 +0200

    scripts: due to change to KERNEL device in udev alsa rule the ATTR command needs a 'device/' prefix

--- resources/scripts/linux/configure/create_udev_alsa.sh
+++ resources/scripts/linux/configure/create_udev_alsa.sh
@@ -2,10 +2,10 @@
 
 DEVICE_ID=${1:-1}
 echo "HAL9000: Configuring udev for ALSA:HAL9000..."
-sudo sh -c 'echo "SUBSYSTEM!=\"sound\", GOTO=\"hal9000_alsa_end\""           >  /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_alsa_end\""         >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "KERNEL==\"controlC'${DEVICE_ID}'\", ATTR{id}=\"HAL9000\""  >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "LABEL=\"hal9000_alsa_end\""                                >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "SUBSYSTEM!=\"sound\", GOTO=\"hal9000_alsa_end\""                   >  /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_alsa_end\""                 >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "KERNEL==\"controlC'${DEVICE_ID}'\", ATTR{/device/id}=\"HAL9000\""  >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "LABEL=\"hal9000_alsa_end\""                                        >> /etc/udev/rules.d/99-hal9000-alsa.rules'
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 
