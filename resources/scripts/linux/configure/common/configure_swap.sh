65568d9f69b83227b6e2993c82375d5533970a7f
Juergen Pabel <juergen@pabel.net>
2024-06-01 23:23:42 +0200

    scripts: vm.swappiness=1 instead of 0 for RPI ZeroW and others because 0 disables swap entirely

--- resources/scripts/linux/configure/common/configure_swap.sh
+++ resources/scripts/linux/configure/common/configure_swap.sh
@@ -4,7 +4,7 @@ SWAPSIZE_MB=${1:-1024}
 if [ "$SWAPSIZE_MB" != "0" ]; then
 	sudo sed -i "s/^CONF_SWAPSIZE=[[:digit:]]\\+\$/CONF_SWAPSIZE=${SWAPSIZE_MB}/" /etc/dphys-swapfile
 	sudo systemctl restart dphys-swapfile.service
-	sudo sh -c "echo 'vm.swappiness=0' > /etc/sysctl.d/99-hal9000-swappiness.conf"
+	sudo sh -c "echo 'vm.swappiness=1' > /etc/sysctl.d/99-hal9000-swappiness.conf"
 else
 	sudo systemctl stop    dphys-swapfile.service
 	sudo systemctl disable dphys-swapfile.service
