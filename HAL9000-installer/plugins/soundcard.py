8691cd9748085d8a5ea2b84476d9a69386939d4d
Juergen Pabel <juergen@pabel.net>
2024-06-03 13:32:37 +0200

    soundcard: added RPI with respeaker handling

--- HAL9000-installer/plugins/soundcard.py
+++ HAL9000-installer/plugins/soundcard.py
@@ -1,3 +1,4 @@
+import os
 import alsaaudio 
 from textual.app import App
 from textual.widget import Widget
@@ -10,14 +11,18 @@ class Soundcard(Plugin):
 		super().__init__(id, name, app)
 
 	def build(self) -> Widget:
-		default_value = Select.BLANK
-		cards = alsaaudio.cards()
 		options = []
-		for pos in range(0, len(cards)):
-			name = cards[pos]
-			value = str(pos)
-			options.append((name, value))
-			if name == 'HAL9000':
-				default_value = value
+		cards = alsaaudio.cards()
+		if os.getenv('HAL9000_SYSTEM_ID', default='') == 'raspberrypi-zero2w' and len(cards) == 1 and cards[0] == 'vc4hdmi':
+			options.append(('seeed-voicecard (driver will be installed automatically)', '0'))
+			default_value = '0'
+		else:
+			default_value = Select.BLANK
+			for pos in range(0, len(cards)):
+				name = cards[pos]
+				value = str(pos)
+				options.append((name, value))
+				if name == 'HAL9000':
+					default_value = value
 		return Select(options, id=self.id, name=self.name, value=default_value, allow_blank=False)
 
