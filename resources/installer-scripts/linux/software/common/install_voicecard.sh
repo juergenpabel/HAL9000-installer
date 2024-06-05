69fc2010625b83af388c2bc011a9152fb07a76bf
Juergen Pabel <juergen@pabel.net>
2024-06-06 00:56:51 +0200

    installer-scripts: update ALSA ID for /etc/asound.conf in install_voicecard.sh

--- resources/installer-scripts/linux/software/common/install_voicecard.sh
+++ resources/installer-scripts/linux/software/common/install_voicecard.sh
@@ -10,8 +10,17 @@ if [ $? -ne 0 ]; then
 	sudo apt install -q -y dkms
 fi
 
-dkms status | grep "^seeed-voicecard/" | grep -q "installed$"
-if [ $? -ne 0 ]; then
+if [ -f /etc/voicecard/dkms.conf ]; then
+	export `grep "^PACKAGE_NAME="    /etc/voicecard/dkms.conf`
+	export `grep "^PACKAGE_VERSION=" /etc/voicecard/dkms.conf`
+else
+	export PACKAGE_NAME="seeed-voicecard"
+	export PACKAGE_VERSION=""
+fi
+dkms status | grep "^${PACKAGE_NAME}/${PACKAGE_VERSION}" | grep -q "installed$"
+if [ $? -eq 0 ]; then
+	echo "NOTICE:  Already installed"
+else
 	if [ ! -d "${GIT_REPODIR}/resources/repositories/seeed-voicecard" ]; then
 		git clone https://github.com/HinTak/seeed-voicecard "${GIT_REPODIR}/resources/repositories/seeed-voicecard"
 	fi
@@ -30,7 +39,8 @@ if [ $? -ne 0 ]; then
 	sudo ./install.sh
 	# notes: 1. install dkms module on all installed kernels
         #        2. dkms install with '--force' neccessary to replace non-functional module 'snd_soc_wm8960' from packaged kernel
-	. /etc/voicecard/dkms.conf
+	export `grep "^PACKAGE_NAME="    /etc/voicecard/dkms.conf`
+	export `grep "^PACKAGE_VERSION=" /etc/voicecard/dkms.conf`
 	find /boot/ -maxdepth 1 -name 'vmlinuz-*v8' | sed 's#/boot/vmlinuz-##g' | while read KERNEL_VERSION ; do
 		sudo dkms install --force ${PACKAGE_NAME}/${PACKAGE_VERSION} -k ${KERNEL_VERSION}
 	done
@@ -38,7 +48,9 @@ if [ $? -ne 0 ]; then
 	if [ $? -eq 1 ]; then
 		sudo sh -c 'echo "autoinstall_all_kernels=\"yes\"" >> /etc/dkms/framework.conf'
 	fi
-else
-	echo "NOTICE:  Already installed"
+	# notes: 1. update /etc/asound.conf (symlink to /etc/voicecard/asound_2mic.conf) for changed ALSA ID
+	#        2. /etc/voicecard is a git repo, add and commit
+	sudo sed -i 's/seeed2micvoicec/HAL9000/g' /etc/asound.conf
+	sudo sh -c 'cd /etc/voicecard ; git add asound_2mic.conf ; git commit -m "HAL9000: changed ALSA ID"'
 fi
 
