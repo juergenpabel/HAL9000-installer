af766a7f2152b024c93d149a86827d5c469c5533
Juergen Pabel <juergen@pabel.net>
2024-06-12 12:20:12 +0200

    added volume mounts for service configurations to kalliope,frontend,brain and console (mounted over configuration inside container image)

---
+++ resources/installer-scripts/container/build/prepare_configuration_repositories.sh
@@ -0,0 +1,1 @@
+../common/prepare_configuration_repositories.sh
---
+++ resources/installer-scripts/container/common/prepare_configuration_repositories.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+CONFIGURATION_GIT_URL=${1:-unknown}
+
+if [ "${CONFIGURATION_GIT_URL}" = "unknown" ]; then
+	echo "Usage: $0 <CONFIG-GIT-URL>"
+	echo "       - CONFIG-GIT-URL: most likely 'https://github.com/juergenpabel/HAL9000-demo-en_US.git' (or other language)"
+        exit 1
+fi
+GIT_NAME=`echo ${CONFIGURATION_GIT_URL} | sed 's#/$##' | sed 's/.git$//' | rev | cut -d'/' -f1 | rev`
+
+SCRIPT_SRC=`realpath "$0"`
+SCRIPT_DIR=`dirname "${SCRIPT_SRC}"`
+
+echo "HAL9000: Cloning git repository '${CONFIGURATION_GIT_URL}' to '~hal9000/HAL9000/${GIT_NAME}'..."
+sudo -i -u hal9000 sh -c "mkdir -p ~hal9000/HAL9000"
+sudo -i -u hal9000 sh -c "git clone ${CONFIGURATION_GIT_URL} ~hal9000/HAL9000/${GIT_NAME}"
+for SERVICE in kalliope brain console frontend ; do
+	echo "HAL9000: Copying git repository to '~hal9000/HAL9000/${SERVICE}'..."
+	sudo -i -u hal9000 sh -c "cp -r ~hal9000/HAL9000/${GIT_NAME} ~hal9000/HAL9000/${SERVICE}"
+	sudo -i -u hal9000 sh -c "cd ~hal9000/HAL9000/${SERVICE} ; git checkout ${SERVICE}"
+done
+
