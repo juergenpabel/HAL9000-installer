a5ce1a73e1b73a01a1458668b63ac89d3340c948
Juergen Pabel <juergen@pabel.net>
2024-07-09 00:49:47 +0200

    create_udev_alsa.sh: added /dev/snd/timer

--- resources/installer-scripts/linux/configure/create_udev_alsa.sh
+++ resources/installer-scripts/linux/configure/create_udev_alsa.sh
@@ -9,13 +9,16 @@ if [ "${ALSA_ID}" = "unknown" ]; then
 fi
 
 echo "HAL9000: Configuring udev for ALSA:HAL9000..."
-sudo sh -c 'echo "SUBSYSTEM!=\"sound\", GOTO=\"hal9000_alsa_end\""                                                           >  /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_alsa_end\""                                                         >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"card?\",    RUN+=\"/usr/bin/mkdir -p /dev/snd/HAL9000\""             >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"controlC?\",RUN+=\"/usr/bin/ln -f /dev/snd/%k /dev/snd/HAL9000/%k\"" >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"pcmC?D?c\", RUN+=\"/usr/bin/ln -f /dev/snd/%k /dev/snd/HAL9000/%k\"" >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"pcmC?D?p\", RUN+=\"/usr/bin/ln -f /dev/snd/%k /dev/snd/HAL9000/%k\"" >> /etc/udev/rules.d/99-hal9000-alsa.rules'
-sudo sh -c 'echo "LABEL=\"hal9000_alsa_end\""                                                                                >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+
+sudo sh -c 'echo "SUBSYSTEM!=\"sound\", GOTO=\"hal9000_alsa_end\""                                                                 >  /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ACTION!=\"add|change\", GOTO=\"hal9000_alsa_end\""                                                               >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"card?\",    RUN+=\"/usr/bin/mkdir -p             /dev/snd/HAL9000\""       >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"card?\",    RUN+=\"/usr/bin/ln -f /dev/snd/timer /dev/snd/HAL9000/timer\"" >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"controlC?\",RUN+=\"/usr/bin/ln -f /dev/snd/%k    /dev/snd/HAL9000/%k\""    >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"pcmC?D?c\", RUN+=\"/usr/bin/ln -f /dev/snd/%k    /dev/snd/HAL9000/%k\""    >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "ATTRS{id}==\"'${ALSA_ID}'\",KERNEL==\"pcmC?D?p\", RUN+=\"/usr/bin/ln -f /dev/snd/%k    /dev/snd/HAL9000/%k\""    >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+sudo sh -c 'echo "LABEL=\"hal9000_alsa_end\""                                                                                      >> /etc/udev/rules.d/99-hal9000-alsa.rules'
+
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 
