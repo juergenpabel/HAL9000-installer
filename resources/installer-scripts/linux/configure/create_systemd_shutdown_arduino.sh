bf28c876ef7e86db594fd10c1af5fc3f6b336e33
Juergen Pabel <juergen@pabel.net>
2024-09-25 11:12:11 +0200

    added linux software configure step for arduino poweroff/reset (new arduino shutdown sequence)

---
+++ resources/installer-scripts/linux/configure/create_systemd_shutdown_arduino.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "HAL9000: Creating systemd shutdown script for arduino poweroff..."
+if [ ! -f /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown ]; then
+	( echo '#!/bin/bash' ; \
+	  echo 'socat EXEC:"echo '\''[\"application/environment\", {\"set\": {\"key\": \"gui/screen:animations/loop\", \"value\": \"false\"}}]'\''" OPEN:/dev/ttyHAL9000' \
+	) | sudo tee /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown > /dev/null
+	sudo chmod 755 /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown
+fi
