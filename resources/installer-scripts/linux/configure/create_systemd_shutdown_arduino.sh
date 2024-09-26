793408e8d7db557e3875c0794bd49735f0ba6ce5
Juergen Pabel <juergen@pabel.net>
2024-09-26 11:41:49 +0200

    changed arduino poweroff/reboot command

--- resources/installer-scripts/linux/configure/create_systemd_shutdown_arduino.sh
+++ resources/installer-scripts/linux/configure/create_systemd_shutdown_arduino.sh
@@ -3,7 +3,7 @@
 echo "HAL9000: Creating systemd shutdown script for arduino poweroff..."
 if [ ! -f /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown ]; then
 	( echo '#!/bin/bash' ; \
-	  echo 'socat EXEC:"echo '\''[\"application/environment\", {\"set\": {\"key\": \"gui/screen:animations/loop\", \"value\": \"false\"}}]'\''" OPEN:/dev/ttyHAL9000' \
+	  echo 'socat EXEC:"echo '\''[\"application/runtime\", {\"SYSTEM:EXIT\": {}}]'\''" OPEN:/dev/ttyHAL9000' \
 	) | sudo tee /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown > /dev/null
 	sudo chmod 755 /lib/systemd/system-shutdown/HAL9000-arduino-poweroff.shutdown
 fi
